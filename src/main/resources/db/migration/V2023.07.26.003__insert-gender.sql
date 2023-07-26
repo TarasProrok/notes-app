184a8ad7e2721ed20b82a322239c7e6c518d1bca
bit-to-bit <vasiliy.pykhteev@gmail.com>
2023-07-26 19:48:16 +0300

    Add DB migrations with flyway. DB migrations add two schemas "access" and "notes", create main tables, fill table gender.

---
+++ src/main/resources/db/migration/V2023.07.26.003__insert-gender.sql
@@ -0,0 +1,5 @@
+INSERT INTO access.gender(gender_id, name_ukr, name_eng)
+VALUES (0, 'Не відомо', 'Not known'),
+       (1, 'Чоловіча', 'Male'),
+       (2, 'Жіноча', 'Female'),
+       (9, 'Не застосовується', 'Not applicable');
