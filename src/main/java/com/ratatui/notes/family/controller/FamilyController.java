fc20a11f8acd5bd3d3a1a3f22cfd490da0ddb8a9
Andriy Gaponov <wmconstructor@gmail.com>
2023-07-28 16:30:08 +0300

    continue family

--- src/main/java/com/ratatui/notes/family/controller/FamilyController.java
+++ src/main/java/com/ratatui/notes/family/controller/FamilyController.java
@@ -30,12 +30,25 @@ public class FamilyController {
         family.setTitle("Моя сім'я");
         family.setCode("125і48");
 
-        family = null;
+        //family = null;
 
         modelAndView.addObject("family", family);
         return modelAndView;
     }
 
+    @GetMapping("/edit")
+    public ModelAndView editUserFamily(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("family/edit");
+
+        //modelAndView.addObject("family", familyService.getFamilyById(null)); //TODO get family id from user
+        Family family = new Family();
+        family.setTitle("Моя сім'я");
+        family.setCode("125і48");
+
+        modelAndView.addObject("family", family);
+        return modelAndView;
+    }
 
     @GetMapping("/leave")
     public RedirectView leaveFamily(){
