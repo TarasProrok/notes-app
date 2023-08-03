7c717965c8d7ce643bef72aff35d786d3edc6e7f
Anna Mamysheva <82759145+amamysheva@users.noreply.github.com>
2023-08-03 19:30:13 +0300

    Added GetMapping for /account

--- src/main/java/com/ratatui/notes/user/UserController.java
+++ src/main/java/com/ratatui/notes/user/UserController.java
@@ -3,13 +3,18 @@ package com.ratatui.notes.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 import com.ratatui.notes.utils.Helper;
 
 import java.sql.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 
@@ -21,6 +26,29 @@ public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/account")
+    public ModelAndView getAccountPage(){
+        ModelAndView result = new ModelAndView("/user/account");
+
+        User currentUser = userService.getCurrentUser();
+        UserDTO userDTO = userMapper.mapEntityToDto(currentUser);
+        List<User> familyUsers = userService.getFamilyUsers(currentUser.getFamily());
+        List<UserDTO> usersFamilyDtos = userMapper.mapEntityToDto(familyUsers);
+
+        Map<Integer, String> gender = new HashMap<>();
+        gender.put(0, "Не відомо");
+        gender.put(1, "Чоловіча");
+        gender.put(2, "Жіноча");
+        gender.put(9, "Не застосовується");
+
+        result.addObject("user", userDTO);
+        result.addObject("family", userDTO.getFamily());
+        result.addObject("gender", gender);
+        result.addObject("usersFamilyDtos", usersFamilyDtos);
+        return result;
+    }
+
 
     @PostMapping("/account")
     public RedirectView updateAccount(@RequestParam(value = "oldEmail") String oldEmail,
